2fd86ec7e44c9a5549c4120907009e9c3355f885
Cyrille Perrot <cperrot@ingeniance.fr>
2021-08-18 17:19:04 +0200

    add countdown

---
+++ src/features/countdown/Countdown.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styles from "./Countdown.module.scss";
+import { IProps } from "./IProps";
+
+/**
+ * Generate a countdown element.
+ * Numbers can be defined in scss associate file.
+ *
+ * @returns the countdown
+ */
+export class Countdown extends React.Component<IProps, {time: number}> {
+  maxNum = parseInt(styles.maxNumber);
+  numbers: React.DetailedHTMLProps<React.HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>[] = [];
+  timerID: number | undefined;
+
+  constructor(props:IProps) {
+    super(props);
+    this.state = {
+      time: parseInt(styles.animDuration)
+    }
+    this.handleTimeChange = this.handleTimeChange.bind(this);
+  }
+
+  // Return the remaining duration of the countdown.
+  handleTimeChange(time: number) {
+    this.props.onTimeChange(time);
+    if (time < 0) {
+      window.clearInterval(this.timerID);
+    }
+  }
+
+  componentDidMount() {
+    // Generate countdown values
+    for (let i = this.maxNum; i > 0; i--) {
+      let numberClassName = "num" + i;
+      this.numbers.push(
+        <span key={"num" + i} className={styles[numberClassName]}>{i}</span>
+      );
+    }
+    this.numbers.push(<span key="num0" className={styles.num0}>START !</span>);
+
+    // Define animation remaining duration
+    this.timerID = window.setInterval(() => this.setState(state => { this.handleTimeChange(state.time); return { time: state.time - 1 }}), 1000);
+  }
+
+  render() {
+    return <div className={`${this.props.className} ${styles.number}`}>{this.numbers}</div>;
+  }
+}
